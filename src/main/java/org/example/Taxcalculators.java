05167859cf37b7c26683f79245b25acd903f19a0
Jorge <jorgequiros579@gmail.com>
2021-09-13 21:49:43 -0400

    Initial commit

---
+++ src/main/java/org/example/Taxcalculators.java
@@ -0,0 +1,27 @@
+package org.example;
+import java.util.Scanner;
+import java.lang.Math;
+/**
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Jorge Quiros
+ */
+public class Taxcalculators
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the order amount? ");
+        double orderAmount = input.nextDouble();
+        System.out.print("What is the state? ");
+        String state = input.next();
+        double tax=0;
+        if(state.equalsIgnoreCase("WI")) {
+            System.out.printf("The subtotal is $%.2f\n", orderAmount);
+            tax = (orderAmount*5.5)/100;
+            System.out.printf("The tax is $%.2f\n", tax);
+        }
+        double total = orderAmount + tax;
+        System.out.printf("The total is $%.2f\n", total);
+        input.close();
+    }
+}
